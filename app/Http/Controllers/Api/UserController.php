e81d753eaa76550cdbb60d42325e979e2a377911
Edwin Gavino Soria Antiporta <edwin.soria.rp@viaexperis2.pe>
2026-05-17 22:00:39 -0500

    fix create 2

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1940,7 +1940,7 @@ class UserController extends BaseController
                     )
                 );
 
-                $this->paymentOrderService->totalProductPatrocinio($dataBody->cartList, $userCreated->id, $_paymentOrder);
+                $this->paymentOrderService->totalProductPatrocinio($dataBody->products, $userCreated->id, $_paymentOrder);
 
                 $this->paymentOrderService->confirmPoint($_paymentOrder , $userCreated , $packCurrent);
 
